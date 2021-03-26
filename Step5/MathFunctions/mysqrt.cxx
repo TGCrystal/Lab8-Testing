89de0123856dcf704152ed5ae05d9e00e673f769
TGCrystal <TGCrystal@users.noreply.github.com>
2021-03-26 15:09:25 -0400

    use correct files now

---
+++ Step5/MathFunctions/mysqrt.cxx
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <iostream>
+
+#include "MathFunctions.h"
+
+// a hack square root calculation using simple operations
+double mysqrt(double x)
+{
+  if (x <= 0) {
+    return 0;
+  }
+
+  #if defined(HAVE_LOG) && defined(HAVE_EXP)
+    double result = exp(log(x) * 0.5);
+    std::cout << "Computing sqrt of " << x << " to be " << result
+              << " using log and exp" << std::endl;
+  #else
+    double result = x;
+  #endif
+
+  return result;
+}
--- build/CMakeFiles/static_block_library.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libstatic_block_library.a"
-)
